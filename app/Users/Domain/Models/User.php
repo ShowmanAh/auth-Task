739dcbbcffd75cc18ff2882906015c7d271f73f7
Showman <showman.sh.ahmed@gmail.com>
2021-08-03 15:36:17 +0200

    add migration role table with many to many relation with user

--- app/Users/Domain/Models/User.php
+++ app/Users/Domain/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
+    }
 }
